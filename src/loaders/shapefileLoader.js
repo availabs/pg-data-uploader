7570fe0a50573faadbe1baa7c7ec8f76a7a917c3
Paul Tomchik <pauljtomchik@live.com>
2017-08-15 23:02:41 -0400

    Added '-lco PRECISION=NO' to fallback org2ogr command to handle 'numeric field overflow' errors.

--- src/loaders/shapefileLoader.js
+++ src/loaders/shapefileLoader.js
@@ -10,23 +10,6 @@ const execAsync = promisify(exec)
 
 async function shapeFileLoader ({ dataPath, schema, tableName }) {
 
-  /* Sample output of ogrinfo:
-
-			INFO: Open of `.'
-						using driver `ESRI Shapefile' successful.
-			1: milbase (3D Polygon)
-	*/
-  // const ogrinfo = await execAsync(`ogrinfo ${dataPath}`)
-
-  // const shapeFileInfo = ogrinfo.stdout.split('\n').filter(line => line.match(/\d+:/))
-	
-	// if (shapeFileInfo.length < 1) {
-		// throw new Error(ogrinfo.stderr || `No ogrinfo for ${tableName}`)
-	// } else if (shapeFileInfo.length > 1) {
-		// throw new Error(`The shapefile loader currently does not support multiple shapes per shapefile: ${tableName}`)
-	// }
-	
-
   let cmd =
 		`ogr2ogr -f PostgreSQL "${this.pgConnectStr}" ${dataPath} ` +
 		`-lco SCHEMA=${schema} -lco OVERWRITE=YES -nln ${tableName}`
@@ -38,7 +21,7 @@ async function shapeFileLoader ({ dataPath, schema, tableName }) {
 				throw new Error(output.stderr)
 			}
 		} catch (err) { // If above failed, retry with PROMOTE_TO_MULTI
-			const output = await execAsync(`${cmd} -nlt PROMOTE_TO_MULTI`)
+			const output = await execAsync(`${cmd} -nlt PROMOTE_TO_MULTI -lco PRECISION=NO`)
 			if (output.stderr) {
 				throw new Error(output.stderr)
 			}
